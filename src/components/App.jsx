1c9aa36304b80c31021a6015e29ca7dbbb2eb2f2
Sean McDermott <seanjmac@hotmail.com>
2018-06-29 13:29:01 -0700

    Add menu component and images for menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './HomePage';
 import BeersList from './BeersList';
 import HoursList from './HoursList';
+import MenuList from './MenuList';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 
@@ -16,7 +17,8 @@ function App(){
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/hours' component={HoursList} />
-        <Route exact path = '/beers' component={BeersList} />
+        <Route exact path= '/beers' component={BeersList} />
+        <Route exact path='/menu' component={MenuList} />
       </Switch>
     </div>
   );
---
+++ src/components/MenuList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MenuList() {
+  return(
+    <div>
+      <style jsx>{`
+          div {
+            color: white;
+          }
+      `}</style>
+      <p>Poo</p>
+    </div>
+  );
+}
+
+export default MenuList;
