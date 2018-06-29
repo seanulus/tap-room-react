41c7395673ed0aebd62c1c275fd1364f6d458fdc
Sean McDermott <seanjmac@hotmail.com>
2018-06-29 11:30:58 -0700

    Add image to home page and try to get overlay

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import tapHouse from '../assets/img/tap-house.jpg';
 
 function HomePage() {
   return (
     <div>
       <style jsx>{`
-          color: white;
-    `}</style>
-      <h2>Home</h2>
+          div {
+            color: white;
+          }
+          .tap-image {
+            width: 100%;
+            background: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.4) 100%), url(${tapHouse});
+          }
 
+    `}</style>
+      <div className='tap-image'>
+
+      </div>
     </div>
   );
 }
